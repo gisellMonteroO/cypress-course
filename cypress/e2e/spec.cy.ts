7ed4a15d992baf6d763a95fd9b04e86d50422255
gisellMontero <gisellmonterodos@gmail.com>
2024-03-07 18:15:07 -0500

    se crea comando context y se buscan otros elementos

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,13 +1,32 @@
-describe('home page', () => {
+describe('Home page', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000')
   })
 
-  it('the h1 contains the correct text', () => {
-    cy.get("[data-test='hero-heading']").contains("Testing Next.js Applications with Cypress")
+  context("Hero section", () => {
+    it('the h1 contains the correct text', () => {
+      cy.get("[data-test='hero-heading']").contains("Testing Next.js Applications with Cypress")
+    })
+    
+    it('the features on the homepage are correct',() => {
+      cy.get('dt').eq(0).contains("4 Courses")
+    })
   })
-  
-  it('the features on the homepage are correct',() => {
-    cy.get('dt').eq(0).contains("4 Courses")
+
+  context("Courses section", () => {
+    it('Course: Testing your first netx.js application', () => {
+      cy.getByData('course-0').find('a').contains('Get started').click()
+      cy.location('pathname').should('eq', '/testing-your-first-application')
+    })
+
+    it('Course: Testing Foundations', () => {
+      cy.getByData('course-1').find('a').eq(3).click()
+      cy.location('pathname').should('eq', '/testing-foundations')
+    })
+
+    it('Course: Cypress Fundamental', () => {
+      cy.getByData('course-2').find('a').contains('Get started').click()
+      cy.location('pathname').should('eq', '/cypress-fundamentals')
+    })
   })
 })
